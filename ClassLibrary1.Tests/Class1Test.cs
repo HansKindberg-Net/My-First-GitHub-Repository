cceee0f3ca8d2048db8afff780c95d2274c9e0dc
unknown <Administrator@hk-winsrv2003r2.(none)>
2012-01-04 21:58:23 +0100

    Add to GitHub.

---
+++ ClassLibrary1.Tests/Class1Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass]
+    public class Class1Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
