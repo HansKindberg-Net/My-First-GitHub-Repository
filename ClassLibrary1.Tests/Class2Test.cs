ce5e3ac87a9b8844ea2f95d7d3ab8e5aaae816fc
Hans Kindberg <hans.kindberg@gmail.com>
2012-01-05 17:11:58 +0100

    Test

---
+++ ClassLibrary1.Tests/Class2Test.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary1.Tests
+{
+	[TestClass]
+	public class Class2Test
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+		}
+	}
+}
