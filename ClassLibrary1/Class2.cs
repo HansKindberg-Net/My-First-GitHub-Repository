c8b79e90af7622ad2ce87e437de8139e415a3381
Hans Kindberg <hans.kindberg@gmail.com>
2012-01-05 17:02:32 +0100

    Fixed tabs.

--- ClassLibrary1/Class2.cs
+++ ClassLibrary1/Class2.cs
@@ -2,14 +2,14 @@
 
 namespace ClassLibrary1
 {
-    public class Class2
-    {
-        public void Method1() { }
+	public class Class2
+	{
+		public void Method1() { }
 
 		public void Method2() { }
 
 		public void Method3() { }
 
 		public void Method4() { }
-    }
+	}
 }
